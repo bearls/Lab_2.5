cc8dba42fe7d4ba9c256bdb4b0d35b5c962fa96c
bearls <61299567+bearls@users.noreply.github.com>
2020-02-20 19:17:18 -0600

    Add files via upload

---
+++ ConsoleApp2.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets do math!");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num2 - num1);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num2/num1);
+
+
+
+            Console.WriteLine("Hello");
+            string greeting1 = Console.ReadLine();
+
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num3 * num4 * num5);
+
+
+            int guessedAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("You look younger than" + " " + guessedAge);
+            Console.ReadLine();
+
+
+        }
+    }
+}
